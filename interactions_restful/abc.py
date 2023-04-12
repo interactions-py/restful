3ea8ffe34132a1baa7a9456fa0210268b39e75bf
Damego <danyabatueff@gmail.com>
2023-04-12 19:30:23 +0500

    refactor(abc): use `abstractmethod`; add `__init__` method

--- interactions_restful/abc.py
+++ interactions_restful/abc.py
@@ -1,14 +1,18 @@
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractmethod
 from typing import Callable, Coroutine
 
-__all__ = ("BaseApi")
+__all__ = ("BaseApi", )
 
 
 class BaseApi(ABC):
-    @abstractclassmethod
+    @abstractmethod
+    def __init__(self, host: str, port: int, **kwargs):
+        pass
+
+    @abstractmethod
     def add_route(self, coro: Callable[..., Coroutine], endpoint: str, method: str, **kwargs):
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     async def run(self):
         pass

d85c683cc062cd18b7dd8247f6d107a9c632302b
Damego <danyabatueff@gmail.com>
2023-04-12 21:18:01 +0500

    refactor(flask): remove useless imports; yes it will warn you everytime

--- interactions_restful/backends/flask_api.py
+++ interactions_restful/backends/flask_api.py
@@ -1,12 +1,6 @@
 from typing import Callable, Coroutine
 
-try:
-    from asgiref.wsgi import WsgiToAsgi
-    from hypercorn.config import Config
-    from hypercorn.asyncio import serve
-    from flask import Flask, Blueprint
-except ImportError as e:
-    raise ImportError("Flask dependencies weren't installed. Please, install they with [flask] option") from e
+from flask import Flask, Blueprint
 
 from ..abc import BaseApi, BaseRouter
 
@@ -25,10 +19,9 @@ class FlaskAPI(BaseApi):
     def __init__(self, host: str, port: int, **kwargs):
         self.host = host
         self.port = port
+        self.debug = kwargs.pop("debug", False)
 
         self.app: Flask = Flask(kwargs.pop("import_name", None) or "interactions_restful", **kwargs)
-        self._config = Config()
-        self._config.bind = [f"{host}:{port}"]
 
     def add_endpoint_method(self, coro: Callable[..., Coroutine], endpoint: str, method: str, **kwargs):
         self.app.route(endpoint, methods=[method], **kwargs)(coro)
@@ -41,4 +34,4 @@ class FlaskAPI(BaseApi):
         self.app.register_blueprint(router.blueprint)
 
     async def run(self):
-        await serve(WsgiToAsgi(self.app), self._config)
+        self.app.run(self.host, self.port, self.debug)

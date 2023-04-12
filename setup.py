3fa6577210e0ed320a46623664d2c63dc678b234
Damego <danyabatueff@gmail.com>
2023-04-12 21:27:56 +0500

    build: update requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ from setuptools import find_packages, setup
 with open("pyproject.toml", "rb") as f:
     pyproject = tomli.load(f)
 
+extras_require = {
+    "flask": ["flask>=2.0.0,<=3.0.0"],
+    "fastapi": ["fastapi>=0.92.0", "uvicorn>=0.20.0"]
+}
 
 setup(
     name=pyproject["tool"]["poetry"]["name"],
@@ -16,12 +20,13 @@ setup(
     description=pyproject["tool"]["poetry"]["description"],
     include_package_data=True,
     install_requires=(Path(__file__).parent / "requirements.txt").read_text().splitlines(),
+    extras_require=extras_require,
     license=pyproject["tool"]["poetry"]["license"],
     long_description=(Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
     url="https://github.com/interactions-py/restful",
     packages=find_packages(),
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
@@ -34,5 +39,5 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Libraries",
         "Topic :: Utilities",
-    ],
+    ]
 )

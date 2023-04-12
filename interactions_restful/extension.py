cd51bba03a69e17a96a4a89d32e076eb8e1eda37
Damego <danyabatueff@gmail.com>
2023-04-12 19:32:23 +0500

    feat: run api in own thread; remove overload methods; use api type

--- interactions_restful/extension.py
+++ interactions_restful/extension.py
@@ -1,63 +1,42 @@
-from asyncio import iscoroutinefunction
+import asyncio
+from threading import Thread
 from inspect import getmembers
-from typing import overload, Literal
+from typing import Type, TypeVar
 
-from interactions.models.internal import Extension
 from interactions import Client
 
 from .abc import BaseApi
 
 __all__ = ("APIExtension", )
+API_TYPE = TypeVar("API_TYPE", bound=BaseApi)
 
 
-class APIExtension(Extension):
+class APIClient:
     bot: Client
 
-    @overload
-    def __init__(
-        self,
-        bot: Client,
-        host: str,
-        port: int, 
-        mode: Literal["flask"],
-        *,
-        import_name: str | None = None,
-        **kwargs
-    ):
-        pass
-
-    @overload
-    def __init__(
-        self,
-        bot: Client,
-        host: str,
-        port: int, 
-        mode: Literal["fastapi"], 
-        **kwargs
-    ):
-        pass
-
-    def __init__(self, bot, host: str, port: int, mode: str, **kwargs) -> None:
+    def __init__(self, bot: Client, api: Type[API_TYPE], host: str, port: int, **kwargs) -> None:
+        self.bot = bot
         self.bot.api = self
 
-        self.client: BaseApi = None
-        match mode:
-            case "flask":
-                from ._flask import FlaskAPI
-                self.client = FlaskAPI(host, port, **kwargs)
-            case "fastapi":
-                from ._fastapi import FastAPI
-                self.client = FastAPI(host, port, **kwargs)
+        self.client: API_TYPE = api(host, port, **kwargs)
 
         self.bot.async_startup_tasks.append((self.run, (), {}))
 
     async def run(self):
+        self._register_extension_routes()
+
+        thread = Thread(target=self._run_api_thread_worker)
+        thread.start()
+
+    def _run_api_thread_worker(self):
+        asyncio.run(self.client.run())
+
+    def _register_extension_routes(self):
         for extension in self.bot.ext.values():
-            for _, coro in getmembers(extension, predicate=iscoroutinefunction):
+            for _, coro in getmembers(extension, predicate=asyncio.iscoroutinefunction):
                 if not hasattr(coro, "__api__"):
                     continue
                 data = coro.__api__
                 self.client.add_route(coro, data["endpoint"], data["method"], **data["kwargs"])
 
-        await self.client.run()
 

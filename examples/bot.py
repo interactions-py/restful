48f6a725f29dd13d307e6437689e3d5a49b39914
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2023-08-09 17:21:31 -0400

    docs: fix example

--- examples/bot.py
+++ examples/bot.py
@@ -1,16 +1,15 @@
 import interactions
+from fastapi import FastAPI
 
-from interactions_restful import setup
 from interactions_restful.backends.fastapi import FastAPIHandler
 
-client = interactions.Client()
-setup(client, FastAPIHandler, "localhost", 8000)
+app = FastAPI()
+client = interactions.Client(token="TOKEN")
+FastAPIHandler(client, app)
+
 client.load_extension("exts.my_ext")
 
 
 @interactions.listen()
 async def on_startup():
     print(f"Bot `{client.user.username}` started up")
-
-
-client.start("TOKEN")
